bdf10beb2a468c89b0925232c930ba8e3080acfe
brianalbin3 <brianalbin3@gmail.com>
2016-07-09 23:22:59 -0400

    Add unit tests for feedreader

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -9,6 +9,7 @@
  * to ensure they don't run until the DOM is ready.
  */
 $(function() {
+    setTimeout(function() {}, 8000);
     /* This is our first test suite - a test suite just contains
     * a related set of tests. This suite is all about the RSS
     * feeds definitions, the allFeeds variable in our application.
@@ -31,30 +32,60 @@ $(function() {
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
+         it('should have defined and not empty URLS', function() {
+            allFeeds.forEach(function(feed) {
+                expect(feed.url).toBeDefined();
+                expect(feed.url).not.toBe("");
+            });
+         });
 
 
         /* TODO: Write a test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
+         it('should have feed names that are defined and not empty', function() {
+            allFeeds.forEach(function(feed) {
+                expect(feed.name).toBeDefined();
+                expect(feed.name).not.toBe("");
+            });
+         });
     });
 
 
     /* TODO: Write a new test suite named "The menu" */
-
+    describe('The menu', function() {
+        var menu =
         /* TODO: Write a test that ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
          * the CSS to determine how we're performing the
          * hiding/showing of the menu element.
          */
+         it('should be hidden by default', function() {
+            expect( $('body').hasClass('menu-hidden') ).toBe(true);
+         });
 
          /* TODO: Write a test that ensures the menu changes
           * visibility when the menu icon is clicked. This test
           * should have two expectations: does the menu display when
           * clicked and does it hide when clicked again.
           */
+        it('changes visibility when the menu icon is clicked', function() {
+            $('.menu-icon-link').trigger('click');
+            expect( $('body').hasClass('menu-hidden') ).toBe(false);
+
+            $('.menu-icon-link').trigger( "click" );
+            expect($('body').hasClass('menu-hidden') ).toBe(true);
+        });
+    });
 
     /* TODO: Write a new test suite named "Initial Entries" */
+    describe('Initial Entries', function() {
+        beforeEach(function(done) {
+            loadFeed(0, function() {
+                done();
+            });
+        });
 
         /* TODO: Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
@@ -62,11 +93,32 @@ $(function() {
          * Remember, loadFeed() is asynchronous so this test will require
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
+        it('contains at least a single .entry element within the .feed container', function(done) {
+            var length = $('.feed').children('.entry-link').length;
+            expect(length).not.toBe(0);
+            done();
+        });
 
-    /* TODO: Write a new test suite named "New Feed Selection"
 
+    });
+    /* TODO: Write a new test suite named "New Feed Selection" */
+    describe('New Feed Selection', function() {
+    var htmlPreLoaded = $('.feed').html();
+    var htmlPostLoaded;
+
+        beforeEach(function(done) {
+            loadFeed(0, function() {
+                htmlPostLoaded = $('.feed').html();
+                done();
+            });
+        });
         /* TODO: Write a test that ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
+        it('changes content when a new feed is loaded', function(done) {
+            expect(htmlPreLoaded).not.toBe(htmlPostLoaded);
+            done();
+        });
+    });
 }());
